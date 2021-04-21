2463c4188a7dd1c4fc1d2a52553d21ca61ead769
HiroakiMorita <taiyou3291@outlook.jp>
2021-04-21 15:42:48 +0900

    third commit

--- api/cardboard/cardboard/settings.py
+++ api/cardboard/cardboard/settings.py
@@ -10,17 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# 環境変数読み込み
+env = environ.Env()
+env.read_env('.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&)uy41ufzg0urqx*zk!rc$!6q#9riaes-ok-gwnyx%$4r_=6a$'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
